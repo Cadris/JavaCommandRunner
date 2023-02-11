5d1a911c6daf9209f5df0339b668a0aa92eb6ddd
Cadris <cadrissilver@gmail.com>
2023-02-12 01:04:48 +0530

    Innitial Commit (New Project)

---
+++ src/main/java/com/sayan/JavaCommandRunner/App.java
@@ -0,0 +1,13 @@
+package com.sayan.JavaCommandRunner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
