ba8b56b12ea159f3086dac6894687946614ec666
Cadris <cadrissilver@gmail.com>
2023-02-12 12:35:04 +0530

    Squashed commit of the following:
    commit 5aabcb80b3569a996fd2fe86d1e8ee53ea06f158
    Author: Cadris <cadrissilver@gmail.com>
    Date:   Sun Feb 12 12:33:10 2023 +0530
    
        Setting Logger and Created Builder and Runner
    
    commit e5b03743e62b523c9bbb5a4960abd3d3e5a30f73
    Author: Cadris <cadrissilver@gmail.com>
    Date:   Sun Feb 12 01:49:50 2023 +0530
    
        gitignore modified
    
    commit 76c153efb70ca6809335a7e8f09251e2d28ea287
    Author: Cadris <cadrissilver@gmail.com>
    Date:   Sun Feb 12 01:47:28 2023 +0530
    
        Basic MAVEN build enabled

--- src/main/java/com/sayan/JavaCommandRunner/App.java
+++ src/main/java/com/sayan/JavaCommandRunner/App.java
@@ -1,13 +1,20 @@
 package com.sayan.JavaCommandRunner;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	
+	private static final Logger logger = LogManager.getLogger(App.class); 
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        logger.error("error log");
     }
 }
